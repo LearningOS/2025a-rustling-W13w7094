7febc5f9554a474930346038a4061b073367be9b
W13w7094 <wangbin7094@gmail.com>
2025-09-25 04:54:39 +0000

    upadte

--- exercises/algorithm/algorithm1.rs
+++ exercises/algorithm/algorithm1.rs
@@ -70,13 +70,76 @@ impl<T> LinkedList<T> {
         }
     }
 	pub fn merge(list_a:LinkedList<T>,list_b:LinkedList<T>) -> Self
-	{
-		//TODO
-		Self {
-            length: 0,
-            start: None,
-            end: None,
+    where T : PartialOrd,
+    {
+		let mut result = LinkedList::new();
+        let mut a_current = list_a.start;
+        let mut b_current = list_b.start;
+        
+        // 临时指针用于构建结果链表
+        let mut result_tail: Option<NonNull<Node<T>>> = None;
+        
+        while let (Some(a_ptr), Some(b_ptr)) = (a_current, b_current) {
+            let a_node = unsafe { &*a_ptr.as_ptr() };
+            let b_node = unsafe { &*b_ptr.as_ptr() };
+            
+            // 比较两个节点的值，选择较小的加入结果链表
+            let (selected_ptr, next_ptr) = if a_node.val <= b_node.val {
+                let next = a_node.next;
+                (a_ptr, next)
+            } else {
+                let next = b_node.next;
+                (b_ptr, next)
+            };
+            
+            // 将选中的节点加入结果链表
+            if result.start.is_none() {
+                result.start = Some(selected_ptr);
+                result_tail = Some(selected_ptr);
+            } else {
+                unsafe {
+                    (*result_tail.unwrap().as_ptr()).next = Some(selected_ptr);
+                }
+                result_tail = Some(selected_ptr);
+            }
+            
+            // 移动到下一个节点
+            if a_node.val <= b_node.val {
+                a_current = next_ptr;
+            } else {
+                b_current = next_ptr;
+            }
+            
+            result.length += 1;
         }
+        
+        // 处理剩余节点
+        let remaining = if a_current.is_some() { a_current } else { b_current };
+        if let Some(remaining_ptr) = remaining {
+            if result.start.is_none() {
+                result.start = Some(remaining_ptr);
+                result_tail = list_a.end.or(list_b.end);
+                // 计算剩余长度
+                let remaining_length = if a_current.is_some() { list_a.length } else { list_b.length };
+                result.length = remaining_length;
+            } else {
+                unsafe {
+                    (*result_tail.unwrap().as_ptr()).next = Some(remaining_ptr);
+                }
+                result_tail = list_a.end.or(list_b.end);
+                result.length += if a_current.is_some() { list_a.length } else { list_b.length };
+            }
+        } else {
+            result_tail = list_a.end.or(list_b.end);
+        }
+        
+        result.end = result_tail;
+        
+        // 防止原链表释放已合并的节点
+        
+        result
+
+        
 	}
 }
 
