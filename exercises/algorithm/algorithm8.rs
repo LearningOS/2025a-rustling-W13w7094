2b197e4408b44bd4e075c64081ca2aecb64f4eef
W13uuu <W13uuu@noreply.gitcode.com>
2025-09-26 09:42:02 +0800

    update

--- exercises/algorithm/algorithm8.rs
+++ exercises/algorithm/algorithm8.rs
@@ -54,37 +54,34 @@ impl<T> Default for Queue<T> {
 
 pub struct myStack<T>
 {
-	//TODO
+	
 	q1:Queue<T>,
 	q2:Queue<T>
 }
 impl<T> myStack<T> {
     pub fn new() -> Self {
         Self {
-			//TODO
+			
 			q1:Queue::<T>::new(),
 			q2:Queue::<T>::new()
         }
     }
     pub fn push(&mut self, elem: T) {
-        //TODO
-         self.q2.enqueue(elem);
-        // Move all elements from q1 to q2
-        while !self.q1.is_empty() {
-            if let Ok(item) = self.q1.dequeue() {
-                self.q2.enqueue(item);
-            }
-        }
-        // Swap q1 and q2
+       self.q2.enqueue(elem);
+         while !self.q1.is_empty() {
+            let v = self.q1.dequeue().unwrap();
+            self.q2.enqueue(v);
+         }
         std::mem::swap(&mut self.q1, &mut self.q2);
     }
     pub fn pop(&mut self) -> Result<T, &str> {
-        //TODO
-        self.q1.dequeue()
+        if self.q1.is_empty() {
+            return Err("Stack is empty");
+        }
+       self.q1.dequeue()
     }
     pub fn is_empty(&self) -> bool {
-		//TODO
-        self.q1.is_empty()
+		self.q1.is_empty()
     }
 }
 
